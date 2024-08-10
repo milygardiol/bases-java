294e6d98ca14007e4a4233926769c3b965058046
Mily <gardiol72@gmail.com>
2024-08-10 15:59:11 -0300

    first commit

---
+++ Ciclos.java
@@ -0,0 +1,16 @@
+
+//Los ciclos se ejecutarán mientras la condición sea verdadera.
+
+//Ciclo while en JAVA
+
+public class Ciclos {
+    public static void main (String[] args) {
+
+        var contador = 1;
+        while (contador <= 3) {
+            System.out.print("Número: " + contador);
+            contador++;
+        }
+    }
+}
+
